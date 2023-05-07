e23a12c1a3874612b0695d02cd727f7e303172ac
yusuf <kingshakzy@gmail.com>
2023-05-07 22:01:48 +0100

    Unsatisfied finish

--- src/components/faces.jsx
+++ src/components/faces.jsx
@@ -3,29 +3,13 @@ import { GrAdd } from "react-icons/gr"
 import { IconContext } from "react-icons/lib";
 import { useState } from "react";
 export const Faces = (props) =>{
-    const [i, setI]= useState(1)
-    let images = props.can
-    if(images.length === 0)images = document.querySelectorAll(".images")
-    let displayFace = (e) =>{
-        console.log(e.target);
-        let di = e.target.nextElementSibling;
-        console.log(di)
-        di.classList.remove("hidden");
-        images[i].innerHTML=   (`<div onClick={() => displayFace()}  className="text-white relative bg-gray-600 cursor-pointer  click w-28 h-28 rounded-full ">
-        ${<IconContext.Provider value={{color: 'white'}}>
-         <GrAdd></GrAdd>
-        </IconContext.Provider>}
-    
-     </div>`)
-        images[i-1].innerHTML = "";
-       
-        console.log(images[i])
-        setI(i+1)
-    }
+   
+    // if(images.length === 0)setImages(document.querySelectorAll(".images"))
+   
     return(
         <>
            
-             <div onClick={(e) => displayFace(e)}  className="text-white relative bg-gray-600 cursor-pointer  click w-28 h-28 rounded-full ">
+             <div   className="flex items-center justify-center text-white relative bg-gray-600 cursor-pointer  click w-28 h-28 rounded-full ">
                 <IconContext.Provider value={{color: 'white'}}>
                  <GrAdd></GrAdd>
                 </IconContext.Provider>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,41 @@
 import { Navbar } from "../components/navbar";
-import { useState, useEffect, useReducer } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Faces } from "../components/faces";
 import { GrAdd } from "react-icons/gr";
+import { IconContext } from "react-icons/lib";
 export const Home = (props) =>{
-  
-  
+    let circleM = useRef()
+    const [i, setI]= useState(0);
+   
+    
+    let bro = document.createElement("div");
+    
+    bro.classList.add("z-10","bg-gray-600", "w-28", "h-28");
+   
+    let displayFace = (e) =>{
+       
+       
+        let circles = circleM.current.childNodes;
+        // console.log(circles)
+        if(i !== 4){
+            circles[i].classList.remove("hidden");
+            circles[i].innerHTML = "";
+            // circles[i+1].appendChild(bro)
+            // console.log(i);
+            // console.log(circles[i])
+            setI(i+1)
+        }else if(i===4){
+            setI(0);
+            circles[i].classList.remove("hidden");
+            circles[i].innerHTML = "";
+            // circles[0].appendChild(bro);
+            console.log(i);
+            console.log(circles[i])
+        }
+        else{
+           setI(0)
+        }
+    }
     // console.log(images)
     return(
         <>
@@ -13,24 +44,25 @@ export const Home = (props) =>{
                 <header>
                     <h1 className="text-xl text-white">Who's watching?</h1>
                 </header>
-                <div className="h-[30rem] relative mx-8  px-8 border-gray-400 border- w-[30rem] rounded-full">
-                    <div  className="images  relative transition transform translate-x-[10rem] translate-y-1   w-28 h-28 rounded-full bg-red-900">
-                        <Faces can = {props.cant}></Faces>
+                <div ref={circleM}  className="h-[30rem] mx-8  px-8 border-gray-400 border- mt-16 w-[30rem] rounded-full">
+                    <div className="images hidden translate-x-[10rem] w-28 h-28 rounded-full bg-red-900">
+                        <div className="block w-28 h-28 z-10 rounded-full border-gray-400"></div>
                     </div>
-                    <div className="images hidden  relative  w-28 h-28 rounded-full bg-white">
+                    <div  className="images hidden  relative  w-28 h-28 rounded-full bg-white">
+                      <img src="" alt="" />
+                    </div>
+                    <div  className="images hidden translate-x-[20rem] translate-y-[-6.6rem] relative w-28 h-28 rounded-full bg-white">
                    
                     </div>
-                    <div className="images hidden relative translate-y-[-6.7rem] translate-x-[19rem] w-28 h-28 rounded-full bg-white">
-                   
+                    <div  className="images hidden translate-y-[-5rem] translate-x-[4rem]  relative w-28 h-28 rounded-full bg-white">
+                  
                     </div>
-                    <div className="images hidden translate-x-[4rem] translate-y-[-4rem] relative w-28 h-28 rounded-full bg-white">
-                   
+                    <div  className="images hidden relative translate-y-[-11.8rem] translate-x-[16rem]  w-28 h-28 rounded-full bg-white">
+                  
                     </div>
-                    <div className="images hidden translate-x-[15rem] translate-y-[-11rem]  relative  w-28 h-28 rounded-full bg-white">
-                   
+                    <div onClick={()=>displayFace()} className="translate-y-[-10rem] translate-x-[8rem] self-center text-white text-xl flex flex-row items-center justify-center w-40 h-16 bg-gray-600 rounded-2xl">
+                        Add Profile
                     </div>
-                   
-            
                 </div>
          </section>
         </>
