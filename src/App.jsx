e2194ee1246e3438db153686b32bf646a72663ec
yusuf <kingshakzy@gmail.com>
2023-05-07 12:42:03 +0100

    Setting up files

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Home } from './pages/home';
 
 function App() {
  
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,7 @@
+export const Home = () =>{
+    return(
+        <>
+        <h2>This is the home page</h2>
+        </>
+    )
+}
